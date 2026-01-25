8376cb6604849395d9cb7b8f297c9e744e8427ca
bartosz <bartoszrydzewski@outlook.com>
2026-01-25 22:11:47 +0100

    cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Search from '@/app/ui/search';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans bg-slate-700">
+    <div className="flex min-h-screen items-center justify-center  font-sans bg-slate-700">
       <main className="flex min-h-screen w-full flex-col items-center justify-between py-32 px-16  bg-slate-700 sm:items-start">
         <Search />
       </main>
